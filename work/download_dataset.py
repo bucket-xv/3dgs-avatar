9d62e5b662ae4a08f836f2ee6109472173194b25
Chenhao Xu <xv_chen_hao@163.com>
2024-12-04 08:58:29 +0000

    Save

--- work/download_dataset.py
+++
@@ -1,3 +0,0 @@
-
-with open('../gdown/gdown/download_folder.py', 'r') as f:
-    code = f.read().replace('MAX_NUMBER_FILES = 50', 'MAX_NUMBER_FILES = 10000')
--- work/env.sh
+++ work/env.sh
@@ -4,4 +4,4 @@ conda env create -f environment.yml
 conda activate 3dgs-avatar
 conda install nvidia/label/cuda-11.6.2::cuda-toolkit
 # install tinycudann
-pip install git+https://github.com/NVlabs/tiny-cuda-nn/#subdirectory=bindings/torch
+pip install -r requirements.txt
