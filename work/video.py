267c5554f930e7b2af6144f1b1638ea5c1c4c2c5
flaricy <1469766238@qq.com>
2024-12-05 20:16:01 +0800

    modify video.py

--- work/video.py
+++ work/video.py
@@ -20,6 +20,31 @@ def create_video_from_pngs(input_folder, output_video_path, fps=30, format='png'
 
 # Set the output video path and the frames per second (fps)
 
+# use cv2 to convert the images to a video 
+def create_video_from_pngs_with_cv2(input_folder, output_video_path, fps=30, format='png'):
+    import cv2 
+    # Get a list of .png files in the current directory
+    png_files = [f for f in os.listdir(input_folder) if f.endswith('.' + format)]
+    png_files.sort()
+
+    # Read the first image to get the dimensions
+    first_image_path = os.path.join(input_folder, png_files[0])
+    first_image = cv2.imread(first_image_path)
+    height, width, layers = first_image.shape
+
+    # Define the video writer
+    fourcc = cv2.VideoWriter_fourcc(*'mp4v')  # For .mp4 output
+    video = cv2.VideoWriter(output_video_path, fourcc, fps, (width, height))
+
+    # Write each frame
+    for filename in png_files:
+        filepath = os.path.join(input_folder, filename)
+        frame = cv2.imread(filepath)
+        video.write(frame)
+
+    # Release the video writer
+    video.release()
+
 
 # Create the parser
 parser = argparse.ArgumentParser(description='Process some integers.')
@@ -34,4 +59,4 @@ parser.add_argument('-d', '--format', default='png', help='Input file format (de
 args = parser.parse_args()
 
 # Call the function to create the video
-create_video_from_pngs(args.input_folder, args.output, args.fps, args.format)
+create_video_from_pngs_with_cv2(args.input_folder, args.output, args.fps, args.format)
